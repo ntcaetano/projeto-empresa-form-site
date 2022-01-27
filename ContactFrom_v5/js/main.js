1907fe3f38e5fc4beac515597bbee264fd22389d
Natanael Caetano <natacaet@gmail.com>
2022-01-27 16:02:18 -0300

    update

--- ContactFrom_v5/js/main.js
+++ ContactFrom_v5/js/main.js
@@ -42,8 +42,33 @@
         });
     });
 
+    $("#cpfcnpj").keydown(function(){
+        try {
+            $("#cpfcnpj").unmask();
+        } catch (e) {}
 
-    function validate (input) {
+        var tamanho = $("#cpfcnpj").val().length;
+
+        if(tamanho < 11){
+            $("#cpfcnpj").mask("999.999.999-99");
+        } else {
+            $("#cpfcnpj").mask("99.999.999/9999-99");
+        }
+
+        // ajustando foco
+        var elem = this;
+        setTimeout(function(){
+            // mudo a posição do seletor
+            elem.selectionStart = elem.selectionEnd = 10000;
+        }, 0);
+        // reaplico o valor para mudar o foco
+        var currentValue = $(this).val();
+        $(this).val('');
+        $(this).val(currentValue);
+    });
+
+
+   /* function validate (input) {
         if($(input).attr('id') == 'cpf' || $(input).attr('name') == 'cpf') {
             if($(input).val().trim().match(/^([0-9]{2}[\.]?[0-9]{3}[\.]?[0-9]{3}[\/]?[0-9]{4}[-]?[0-9]{2})|([0-9]{3}[\.]?[0-9]{3}[\.]?[0-9]{3}[-]?[0-9]{2})$/) == null) {
                 return false;
